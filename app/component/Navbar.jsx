df2ae9c0402cf07267faf9647ff612c9103471b2
Uzziel <yocorangelo18@gmail.com>
2024-12-02 10:42:26 +0800

    add: popover pero di pa tapos

--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Button, User } from "@nextui-org/react";
 import { auth, signOut } from "../../auth";
+import Link from 'next/link'
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover";
 
 export default async function Navbar() {
  const session = await auth()
@@ -9,12 +11,28 @@ const userAvatar =await String(session.user?.image);
   return (
    <>
     <div className='w-auto border-b lg:p-3 p-2 flex justify-between'>
-     <User   
+       
+        <Popover
+          backdrop="blur"
+            showArrow
+          placement="right">
+              <PopoverTrigger>
+     <User
       name={userName}
       avatarProps={{
         src:userAvatar
       }}
-     />
+      
+              />
+            </PopoverTrigger>
+              <PopoverContent>
+        <div className="px-1 py-2">
+          <div className="text-small font-bold">Popover Content</div>
+          <div className="text-tiny">This is the popover content</div>
+        </div>
+      </PopoverContent>
+            </Popover>
+        
      <div>
        <form
         action={async () => {
---
+++ app/login/profile/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/app/component/Navbar'
+
+
+export default function page () {
+  return (
+   <div className='min-w-max min-h-screen flex items-center justify-center'>
+      <div className='lg:w-[60%] min-h-screen w-screen border-x'>
+       <Navbar/>
+      </div>
+      </div>
+  )
+}
+
