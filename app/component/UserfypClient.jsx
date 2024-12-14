c62d9e17ba57a60639d555c57584fa3162350350
Uzziel <yocorangelo18@gmail.com>
2024-12-14 19:23:48 +0800

    add: name and caption in fyp

--- app/component/UserfypClient.jsx
+++ app/component/UserfypClient.jsx
@@ -1,7 +1,7 @@
 // components/UserfypClient.js
 "use client"
-import React, { useState, useEffect } from 'react';
 import { Image } from "@nextui-org/react";
+import { useEffect, useState } from 'react';
 
 export default function UserfypClient() {
   const [windowWidth, setWindowWidth] = useState(null);
@@ -21,13 +21,19 @@ export default function UserfypClient() {
 
   return (
     <div className="w-auto lg:w-full flex justify-center">
-      <Image
+      <div className=" m-5">
+        <div className="flex flex-col gap-[0.3rem]">
+           <h1 className="font-semibold lg:text-3xl text-4xl">yocorangelo18@gmail.com</h1>
+        <p>this is the caption</p>
+       </div>
+         <Image
         src="https://app.requestly.io/delay/5000/https://nextui.org/images/hero-card-complete.jpeg"
         alt="NextUI hero Image with delay"
         width={isMobile ? 300 : 500}
         height={isMobile ? 200 : 300}
         objectFit="cover"
       />
+     </div>
     </div>
   );
 }
