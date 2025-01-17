5143fa7bbfe88943f2e591d8874f2ee5d849e365
Uzziel <yocorangelo18@gmail.com>
2025-01-17 21:18:33 +0800

    update:save last edit @profile page

---
+++ app/component/ProfileButton.js
@@ -0,0 +1,11 @@
+import {Button} from "@nextui-org/react";
+
+export default function ProfileButton() {
+    return (
+        <>
+            <Button color="danger">
+                profile
+            </Button>
+        </>
+    )
+}
